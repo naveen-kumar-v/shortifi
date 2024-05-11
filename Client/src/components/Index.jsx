5dfaca058b71f49615ae5159a5189959467a6d6e
naveen-kumar-v <naveengv75@gmail.com>
2024-05-12 00:36:36 +0530

    Bug fixes

--- Client/src/components/Index.jsx
+++ Client/src/components/Index.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const Index = () => {
   const [longUrl, setLongUrl] = useState("");
-  const [shortId, setShortId] = useState("");
+  const [shortId, setShortId] = useState(null);
   const baseUrl = import.meta.env.VITE_BASE_URL;
   const headers = { "Content-Type": "application/json" };
 
@@ -19,13 +19,22 @@ const Index = () => {
     }
   };
 
+  const handleCopyUrl = async () => {
+    try {
+      await navigator.clipboard.writeText(`http://localhost:3000/${shortId}`);
+      console.log("URL copied to clipboard successfully!");
+    } catch (err) {
+      console.error("Failed to copy URL to clipboard:", err);
+    }
+  };
+
   return (
     <div className="p-4 md:px-10 lg:px-20 md:py-10 bg-[rgba(255,255,255,0.3)] rounded-lg flex justify-center items-center flex-col gap-8 text-white w-[80vw]">
       <h1>URL Shortner</h1>
 
       <div className="w-full flex flex-col gap-4 md:gap-6 md:flex-row ">
         <div className="flex flex-col w-full">
-          <label htmlFor="longUrl" className="text-lg font-medium ">
+          <label htmlFor="longUrl" className="text-lg font-medium pb-1.5">
             Paste a long Url
           </label>
           <input
@@ -37,37 +46,49 @@ const Index = () => {
           />
         </div>
         <button
-          className=" bg-green-700 px-4 py-1.5 rounded-lg text-green-50 font-medium text-base md:text-lg hover:bg-green-600 active:bg-green-500 transition-all w-[100%] md:w-[35%] self-end"
+          className=" bg-[#172fb6] px-4 py-1.5 rounded-lg text-green-50 font-medium text-base md:text-lg hover:bg-[#143ae1] active:bg-[#2455f5] transition-all w-[100%] md:w-[35%] self-end disabled:opacity-60 disabled:pointer-events-none"
           onClick={shortenUrl}
+          disabled={!longUrl.length}
         >
           Shorten URL
         </button>
       </div>
 
-      <div className="w-full flex flex-col rounded-xl bg-[rgba(255,255,255,0.1)] p-2">
+      <div className="w-full flex flex-col rounded-xl bg-[rgba(255,255,255,0.1)] p-2 py-3">
         <p htmlFor="longUrl" className="text-lg font-medium ps-2 pb-2">
-          Your Shortend Url
+          Shortend Url
         </p>
 
         <div className="flex gap-2 flex-col md:flex-row w-full justify-start items-center">
-          <div className="w-full md:w-[50%] text-base md:text-lg font-medium p-1.5 bg-[rgba(255,255,255,0.2)] rounded">
-            {`www.shortifi.netlify.app/${shortId}`}{" "}
-          </div>
+          {!shortId ? (
+            <p className="text-base md:text-lg text-center w-full pb-4">
+              {" "}
+              No Shortend Urls{" "}
+            </p>
+          ) : (
+            <>
+              <div className="w-full md:w-[50%] text-base md:text-lg font-medium p-1.5 ps-2 bg-[rgba(255,255,255,0.2)] rounded">
+                {`www.shortifi.netlify.app/${shortId}`}{" "}
+              </div>
 
-          <div className="flex gap-4 w-full md:w-[50%]">
-            <button className="bg-green-700 px-4 py-2 rounded-lg text-green-50 font-medium text-base hover:bg-green-600 active:bg-green-500 transition-all flex-1">
-              Copy URL
-            </button>
-            <button className="bg-green-700 px-4 py-2 rounded-lg text-green-50 font-medium text-base hover:bg-green-600 active:bg-green-500 transition-all flex-1">
-              <a
-                href={`http://localhost:3000/${shortId}`}
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                Go to URL
-              </a>
-            </button>
-          </div>
+              <div className="flex gap-4 w-full md:w-[50%]">
+                <button
+                  className="bg-[#172fb6] px-4 py-2 rounded-lg text-green-50 font-medium text-base hover:bg-[#143ae1] active:bg-[#2455f5] transition-all flex-1"
+                  onClick={handleCopyUrl}
+                >
+                  Copy URL
+                </button>
+                <a
+                  className="bg-[#172fb6] px-4 py-2 rounded-lg text-green-50 font-medium text-base hover:bg-[#143ae1] active:bg-[#2455f5] transition-all flex-1 text-center"
+                  href={`http://localhost:3000/${shortId}`}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Go to URL
+                </a>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>

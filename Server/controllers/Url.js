9a28caf9bd6407f3628f27d899edc7384d08b0c8
naveen-kumar-v <naveengv75@gmail.com>
2024-05-12 14:12:03 +0530

    Added analytics

--- Server/controllers/Url.js
+++ Server/controllers/Url.js
@@ -1,6 +1,7 @@
 // import mongoose from "mongoose"; 
 import { nanoid } from 'nanoid';
 import url from '../models/Url.js';
+import validUrl from 'valid-url';
 
 export const getShortUrl = async (req, res) => {
   try {
@@ -12,9 +13,16 @@ export const getShortUrl = async (req, res) => {
       })
     }
 
-    const shortID = nanoid(8);
+    // Check if the URL is valid
+    if (!validUrl.isUri(body.url)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid URL. Please enter a valid URL."
+      });
+    }
 
     const toUrl = body.url.startsWith('http://') || body.url.startsWith('https://') ? body.url : `https://${body.url}`;
+    const shortID = nanoid(6);
 
     const newId = await url.create({
       shortId: shortID,
@@ -55,7 +63,6 @@ export const goToShortUrl = async (req, res) => {
     );
 
     if (!entry) {
-      // console.log("No URL found for this shortId:", shortId);
       return res.status(404).json({
         success: false,
         message: `No URL found for this shortId: ${shortId}`,
@@ -73,4 +80,36 @@ export const goToShortUrl = async (req, res) => {
   }
 };
 
+export const getAnalytics = async (req, res) => {
+  try {
+    const { shortId } = req.params;
+    console.log(shortId);
 
+    const response = await url.findOne({ shortId });
+
+    if (!response) {
+      return res.status(404).json({
+        success: false,
+        message: `No data found for this short Url`,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      // message: "",
+      totalClicks: response?.visitedHistory.length,
+      data: {
+        timestamps: response?.visitedHistory,
+        longUrl: response?.redirectUrl,
+      }
+    });
+    // (entry?.redirectUrl);
+
+  } catch (err) {
+    console.log("Error:", err);
+    res.status(400).json({
+      success: false,
+      message: "Error while redirecting to the original URL.",
+    });
+  }
+};
--- Server/routes/Url.js
+++ Server/routes/Url.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getShortUrl, goToShortUrl } from '../controllers/Url.js';
+import { getShortUrl, goToShortUrl, getAnalytics } from '../controllers/Url.js';
 
 
 const urlRouter = express.Router();
 
 urlRouter.post('/url', getShortUrl)
 urlRouter.get('/:shortId', goToShortUrl)
+urlRouter.get('/analytics/:shortId', getAnalytics)
 
 export default urlRouter;
